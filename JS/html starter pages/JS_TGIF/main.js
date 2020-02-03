4dec5a070b78d26e437d1032905146fe872fe7e1
T11641.Training56 <T11641.Training56@accenture.com>
2020-02-03 16:35:01 -0300

    task 3 js

--- JS/html starter pages/JS_TGIF/main.js
+++ JS/html starter pages/JS_TGIF/main.js
@@ -2,23 +2,95 @@ const members = data.results[0].members
 
 const tbody = document.querySelector("tbody")
 
-members.forEach(member => {
-				
-	let row = tbody.insertRow(-1);
+function checkbox(){
+	document.getElementById("table-data").innerHTML =""
+	let state = document.getElementById("inputState").value
+	let party = document.getElementsByClassName("form-check-input") 
+	let select = []
+	let dataFilter=[]
+	for (let i = 0; i< party.length; i++) {
+		if (party[i].checked) {
+			dataFilter = members.filter(e => e.party == party[i].value)
+			
+			
 
-	let names
+			members.filter(e => e.party == party[i].value && (e.state == state || state == "all")). forEach(member => {
 
-	let fullname = member.last_name + " " + member.first_name + " " + (member.middle_name || " ")
+				let row = tbody.insertRow(-1);
 
-	if (member.url == "") {
+				let nombre
 
-		names = fullname
+				let fullname = member.last_name + " " + member.first_name + " " + (member.middle_name || " ")
+
+				if(member.url == ""){
+				nombre = fullname
+				}
+
+				else{
+				nombre = `<a class="text-white" target:"_blank" href="${member.url}">` + fullname + `</a>`
+				}
+
+				row.innerHTML = "<td>" + nombre + "</td><td>" + member.party + "</td><td>" + member.state + "</td><td>" + member.seniority + "</td><td>" + member.votes_with_party_pct + "</td>"
+			})
+		}
+		dataFilter.forEach(e => {
+			if (select.indexOf(e.state)== (-1)) {
+				select.push(e.state)
+
+			}
+
+		})
+		document.getElementById("inputState").innerHTML = `<option value="all">all</option>`
+		select.forEach(e => {document.getElementById("inputState").innerHTML += `<option value="${e}">${e}</option>`})
+		document.getElementById("inputState").value = state
 	}
+}
+document.getElementById("inlineCheckbox1").addEventListener("click", checkbox)
+document.getElementById("inlineCheckbox2").addEventListener("click", checkbox)
+document.getElementById("inlineCheckbox3").addEventListener("click", checkbox)
+checkbox()
+document.getElementById("inputState").addEventListener("change", checkbox)
 
-	else{
+// function checkbox(){
+// 	tbody.innerHTML =""
+// 	let party = document.getElementsByClassName("form-check-input")
 
-		names = `<a class="text-white" target="_blank" href=${member.url}>` + fullname + `</a>`
-	}
+// 	let filter = members.filter(function(e){
+// 		for(i = 0 ; i<party.length ; i++){
+// 			if(party[i].checked == true && party[i].value == e.party){
+// 				return e
+// 			}
 
-	row.innerHTML = "<td>" + names + "</td><td>" + member.party + "</td><td>" + member.state + "</td><td>" + member.seniority + "</td><td>" + member.votes_with_party_pct + "</td>"
-})
+// 		}
+// 	})
+// filter.forEach(member => {
+// 	// if((member.party == state[0].value && state[0].checked) || (member.party == state[1].value && state[1].checked) || (member.party == state[2].value && state[2].checked)){
+	
+
+// 	let row = tbody.insertRow(-1);
+
+// 	let names
+
+// 	let fullname = member.last_name + " " + member.first_name + " " + (member.middle_name || " ")
+
+// 	if (member.url == "") {
+
+// 		names = fullname
+// 	}
+
+// 	else{
+
+// 		names = `<a class="text-white" target="_blank" href=${member.url}>` + fullname + `</a>`
+// 	}
+
+// 	row.innerHTML = "<td>" + names + "</td><td>" + member.party + "</td><td>" + member.state + "</td><td>" + member.seniority + "</td><td>" + member.votes_with_party_pct + "</td>"
+// }
+// )
+// }
+
+// checkbox()
+
+// let party = document.getElementsByClassName("form-check-input")
+// for(let i=0;i<party.length;i++)
+// 	party[i].addEventListener("click",checkbox)
+
