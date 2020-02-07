8522861fa1474051949098f05a6111a4a2def597
T11641.Training56 <T11641.Training56@accenture.com>
2020-02-07 16:31:30 -0300

    task 3 finalizada

--- JS/html starter pages/JS_TGIF/stats.js
+++ JS/html starter pages/JS_TGIF/stats.js
@@ -4,65 +4,48 @@ const members = data.results[0].members.filter(j => j.total_votes !=0)
 
 const dri = document.getElementById("dri")
 
-let democrats = 0
-
-let republicans = 0
-
-let independents = 0
-
-let votesPartyD = 0
-
-let	votesPartyR = 0
-
-let	votesPartyI = 0
-
-let	votesPartyT = 0
-
-let total = data.results[0].num_results
+let stats={
+	democrats:0,
+	republicans:0,
+	independents:0,
+	votesPartyD:0,
+	votesPartyR:0,
+	votesPartyI:0,
+	total : data.results[0].num_results,
+	totalPct:0,
+}
 
 members.forEach(c => {
 
 		if (c.party == "D") {
-			democrats++
-			votesPartyD += c.votes_with_party_pct
+			stats.democrats++
+			stats.votesPartyD += c.votes_with_party_pct
 		}
 		if (c.party == "R") {
-			republicans++
-			votesPartyR += c.votes_with_party_pct
+			stats.republicans++
+			stats.votesPartyR += c.votes_with_party_pct
 		}
 		if (c.party == "I") {
-			independents++
-			votesPartyI += c.votes_with_party_pct
+			stats.independents++
+			stats.votesPartyI += c.votes_with_party_pct
 		}
-
-		let votesPctD = parseFloat(votesPartyD/democrats).toFixed(2)
-
-		let votesPctR = parseFloat(votesPartyR/republicans).toFixed(2)
-
-		let votesPctI = parseFloat(votesPartyI/independents).toFixed(2)
-
-		let votesPctT = parseFloat((votesPartyD + votesPartyR + votesPartyI) / total).toFixed(2)
-
-		if (votesPartyD == 0) {
-			votesPctD = 0
-		}
-
-		if (votesPartyR == 0) {
-			votesPctR = 0
-		}
-
-		if (votesPartyI == 0) {
-			votesPctI = 0
-		}
-	
-	dri.innerHTML = `<tr> <td> Democrats </td><td> ${democrats} </td><td> ${votesPctD} </td></tr> 
-	<tr><td> Republicans </td><td> ${republicans} </td><td> ${votesPctR} </td></tr> 
-	<tr><td> Independents </td><td> ${independents} </td><td> ${votesPctI} </td></tr>
-	<tr><td> Total </td><td> ${total} </td><td> ${votesPctT} </td></tr>` 
 
 })
 
-// ---------------------------Least Engaged Table------------------------------------
+stats.votesPartyD = + (parseFloat(stats.votesPartyD/stats.democrats).toFixed(2))
+
+stats.votesPartyR = + (parseFloat(stats.votesPartyR/stats.republicans).toFixed(2))
+
+stats.votesPartyI = + (parseFloat(stats.votesPartyI/stats.independents).toFixed(2))
+
+stats.totalPct = parseFloat((stats.votesPartyD + stats.votesPartyR + stats.votesPartyI) / 3).toFixed(2)
+
+	dri.innerHTML = `<tr> <td> Democrats </td><td> ${stats.democrats} </td><td> ${stats.votesPartyD} </td></tr> 
+	<tr><td> Republicans </td><td> ${stats.republicans} </td><td> ${stats.votesPartyR} </td></tr> 
+	<tr><td> Independents </td><td> ${stats.independents} </td><td> ${stats.votesPartyI} </td></tr>
+	<tr><td> Total </td><td> ${stats.total} </td><td> ${stats.totalPct} </td></tr>` 
+
+// ---------------------------Tablas 10%------------------------------------
 
 function porcentaje (array,prop,isAscendet,idTable) {
 	const table = document.getElementById(idTable)
@@ -81,153 +64,66 @@ function porcentaje (array,prop,isAscendet,idTable) {
 
 		}
 
-		sortedArray.forEach(e => {
+		stats[idTable] = sortedArray
 
-		let row = table.insertRow(-1);
+		if (prop == "missed_votes_pct") {
 
-		let nombre
+			sortedArray.forEach(e => {
 
-		let fullname = e.last_name + " " + e.first_name + " " + (e.middle_name || " ")
+			let row = table.insertRow(-1);
 
-		if(e.url == ""){
-			nombre = fullname
+			let nombre
+
+			let fullname = e.last_name + " " + e.first_name + " " + (e.middle_name || " ")
+
+			if(e.url == ""){
+				nombre = fullname
+			}
+
+			else{
+				nombre = `<a class="text-white" target:"_blank" href="${e.url}">` + fullname + `</a>`
+			}
+
+			row.innerHTML = "<td>" + nombre + "</td><td>" + e.missed_votes + "</td><td>" + e[prop] + "</td>"
+
+			})
 		}
 
-		else{
-			nombre = `<a class="text-white" target:"_blank" href="${e.url}">` + fullname + `</a>`
+		else if (prop == "votes_with_party_pct") {
+
+			sortedArray.forEach(e => {
+
+			let row = table.insertRow(-1);
+
+			let nombre
+
+			let fullname = e.last_name + " " + e.first_name + " " + (e.middle_name || " ")
+
+			if(e.url == ""){
+				nombre = fullname
+			}
+
+			else{
+				nombre = `<a class="text-white" target:"_blank" href="${e.url}">` + fullname + `</a>`
+			}
+
+			let least_no_party_votes = parseFloat((e.votes_with_party_pct * e.total_votes) * 0.01).toFixed(0)
+
+			row.innerHTML = "<td>" + nombre + "</td><td>" + least_no_party_votes + "</td><td>" + e[prop] + "</td>"
+
+			})
+
 		}
-
-		row.innerHTML = "<td>" + nombre + "</td><td>" + e.missed_votes + "</td><td>" + e[prop] + "</td>"
-
-})
 	}
-
 
 }
 
-// let pct10 = Math.round(members.length * 0.1)
+porcentaje (members,"missed_votes_pct",false,"least_engaged")
+porcentaje (members,"missed_votes_pct",true,"most_engaged")
+porcentaje (members,"votes_with_party_pct",false,"least_loyal")
+porcentaje (members,"votes_with_party_pct",true,"most_loyal")
 
-// const least_engaged = document.getElementById("least_engaged")
 
-// if (least_engaged) {
-
-// 	let least_engaged_array = members.slice().sort( function(a,b){return b.missed_votes_pct - a.missed_votes_pct}).slice(0,pct10)
-
-// 	least_engaged_array.forEach(e => {
-
-// 		let row = least_engaged.insertRow(-1);
-
-// 		let nombre
-
-// 		let fullname = e.last_name + " " + e.first_name + " " + (e.middle_name || " ")
-
-// 		if(e.url == ""){
-// 			nombre = fullname
-// 		}
-
-// 		else{
-// 			nombre = `<a class="text-white" target:"_blank" href="${e.url}">` + fullname + `</a>`
-// 		}
-
-// 		row.innerHTML = "<td>" + nombre + "</td><td>" + e.missed_votes + "</td><td>" + e.missed_votes_pct + "</td>"
-
-// })
-
-// }
-
-// ---------------------------Most Engaged Table-------------------------------------
-
-// const most_engaged = document.getElementById("most_engaged")
-
-// if (most_engaged) {
-
-// 	let most_engaged_array = members.slice().sort(function(a,b){return a.missed_votes_pct - b.missed_votes_pct}).slice(0, + pct10)
-
-// 	most_engaged_array.forEach(e =>  {
-
-// 		let row = most_engaged.insertRow(-1);
-
-// 		let nombre
-
-// 		let fullname = e.last_name + " " + e.first_name + " " + (e.middle_name || " ")
-
-// 		if(e.url == ""){
-// 			nombre = fullname
-// 		}
-
-// 		else{
-// 			nombre = `<a class="text-white" target:"_blank" href="${e.url}">` + fullname + `</a>`
-// 		}
-
-// 		row.innerHTML = "<td>" + nombre + "</td><td>" + e.missed_votes + "</td><td>" + e.missed_votes_pct + "</td>"
-
-// })
-
-// }
-
-// // ---------------------------Least Loyal Table--------------------------------------------
-
-// const least_loyal = document.getElementById("least_loyal")
-
-// if (least_loyal) {
-
-// 	let least_loyal_array = members.slice().sort(function(a,b){return b.votes_with_party_pct - a.votes_with_party_pct}).slice(0,pct10)
-
-// 	least_loyal_array.forEach(e => {
-
-// 		let row = least_loyal.insertRow(-1);
-
-// 		let nombre
-
-// 		let fullname = e.last_name + " " + e.first_name + " " + (e.middle_name || " ")
-
-// 		if(e.url == ""){
-// 			nombre = fullname
-// 		}
-
-// 		else{
-// 			nombre = `<a class="text-white" target:"_blank" href="${e.url}">` + fullname + `</a>`
-// 		}
-
-// 		let least_no_party_votes = parseFloat((e.votes_with_party_pct * e.total_votes) * 0.01).toFixed(0)
-
-// 		row.innerHTML = "<td>" + nombre + "</td><td>" + least_no_party_votes + "</td><td>" + e.votes_with_party_pct + "</td>"
-
-// })
-
-// }
-
-// // ---------------------------Most Loyal Table-------------------------------------------
-
-// const most_loyal = document.getElementById("most_loyal")
-
-// if (most_loyal) {
-
-// 	let most_loyal_array = members.slice().sort(function(a,b){return a.votes_with_party_pct - b.votes_with_party_pct}).slice(0,pct10)
-
-// 	most_loyal_array.forEach(e => {
-
-// 		let row = most_loyal.insertRow(-1);
-
-// 		let nombre
-
-// 		let fullname = e.last_name + " " + e.first_name + " " + (e.middle_name || " ")
-
-// 		if(e.url == ""){
-// 			nombre = fullname
-// 		}
-
-// 		else{
-// 			nombre = `<a class="text-white" target:"_blank" href="${e.url}">` + fullname + `</a>`
-// 		}
-
-// 		let most_no_party_votes = parseFloat((e.votes_with_party_pct * e.total_votes) / 100).toFixed(0)
-
-// 		row.innerHTML = "<td>" + nombre + "</td><td>" + most_no_party_votes + "</td><td>" + e.votes_with_party_pct + "</td>"
-
-// })
-
-// }
 
 // // ---------------------------Least Engaged Table------------------------------------
 
@@ -360,16 +256,3 @@ function porcentaje (array,prop,isAscendet,idTable) {
 // })
 
 // }
-
-
-
-// function mf(pro,isAscendet)
-
-
-// mf("votes_with_party_pct",false)
-
-
-
-
-
- 
